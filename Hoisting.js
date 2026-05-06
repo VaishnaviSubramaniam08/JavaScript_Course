df35fcae5cdaaf0286aab3cd3866f1082e8297e2
Vaishnavi S <vaishnavisubramaniam247@gmail.com>
2026-05-06 14:57:41 +0530

    Hoisting

---
+++ Hoisting.js
@@ -0,0 +1,26 @@
+console.log(a);
+let a=100;
+
+console.log(a);
+var a=100;
+
+let a;
+console.log(a);
+
+var a;
+console.log(a);
+
+console.log(a);
+var a=100;
+console.log(a);
+
+greet();
+function greet(){
+    console.log("Welcome maa");
+}
+
+greet();
+var greet=function(){
+    console.log("Welcome maa");
+}
+
