5cfab125db13c1a857b16753ecc600cd9fbc6ff0
Vaishnavi S <vaishnavisubramaniam247@gmail.com>
2026-05-31 14:47:18 +0530

    js

---
+++ boolean.js
@@ -0,0 +1,6 @@
+// Convert any value to boolean
+let value = "hello";
+let bool = Boolean(value);  // true
+console.log(bool);  // true
+let shortcut = !!value;
+console.log(shortcut);     // true (double negation trick)
